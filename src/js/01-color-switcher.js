2cd1dfe80ad412419ddec2f67e573ab7aef44165
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2023-12-06 10:52:08 +0200

    save one

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 
+let intervalId = null;
+
+startButton.addEventListener('click', startChangingBackgroundColor);
+stopButton.addEventListener('click', stopChangingBackgroundColor);
+
+function startChangingBackgroundColor() {
+  startButton.disabled = true;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopChangingBackgroundColor() {
+  startButton.disabled = false;
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
