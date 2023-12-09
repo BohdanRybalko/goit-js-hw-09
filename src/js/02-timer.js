45a7a73897be147f47cb14575ccc31b2747e0ff0
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2023-12-10 01:12:45 +0200

    run

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,11 +1,11 @@
 import flatpickr from 'flatpickr/dist/flatpickr.min.js';
 import 'flatpickr/dist/flatpickr.min.css';
 
+const startButton = document.querySelector('[data-start]');
 const daysElement = document.querySelector('[data-days]');
 const hoursElement = document.querySelector('[data-hours]');
 const minutesElement = document.querySelector('[data-minutes]');
 const secondsElement = document.querySelector('[data-seconds]');
-const startButton = document.querySelector('[start-button]');
 
 const options = {
   enableTime: true,
